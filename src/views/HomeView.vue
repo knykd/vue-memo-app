38c1c76289abbbbb6ce20db0d46ba2786b8d155c
kenya.okada <kenya.okada@kenyaokadanoMacBook-Pro.local>
2023-12-16 01:19:39 +0900

    add memo form page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an HomeView page</h1>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
