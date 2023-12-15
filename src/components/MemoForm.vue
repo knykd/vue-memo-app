a79a08f6fc2bb5bb9781e23aef0a86cd222761a1
kenya.okada <kenya.okada@kenyaokadanoMacBook-Pro.local>
2023-12-16 01:47:33 +0900

    save state

--- src/components/MemoForm.vue
+++ src/components/MemoForm.vue
@@ -1,31 +1,51 @@
 <template>
   <h2>New</h2>
-  <div><input type="text"></div>
-  <div><textarea></textarea></div>
+  <div><input type="text" v-model="title"></div>
+  <div><textarea v-model="content"></textarea></div>
   <div class="center">
-    <button>保存</button>
+    <button @click="save">保存</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MemoForm'
+  name: 'MemoForm',
+  data() {
+    return {
+      title: '',
+      content: ''
+    }
+  },
+  methods: {
+    save() {
+      let memo = {
+        title: this.title,
+        content: this.content
+      }
+
+      this.$store.commit('save', memo)
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
 <style scoped>
 div {
-  margin-bottom: 10px;
+    margin-bottom: 10px;
 }
 input[type=text] {
-  width: 100%;
-  height: 30em;
+    width: 100%;
+}
+textarea {
+    width: 100%;
+    height: 30em;
 }
 button {
-  width: 5em;
-  margin: 3px
+    width: 5em;
+    margin: 3px;
 }
 .center {
-  text-align: center;
+    text-align: center;
 }
 </style>
